b1b232c8c98226242ff301f2ae73ff1cdaaef0d6
Lucas <lucasphone75@gmail.com>
2025-03-27 17:55:15 -0600

    debug wal

--- server/etcdserver/raft.go
+++ server/etcdserver/raft.go
@@ -285,7 +285,7 @@ func (r *raftNode) start(rh *raftReadyHandler) {
 				}
 
 				r.raftStorage.Append(rd.Entries)
-				fmt.Printf("Applied incoming Raft entries\n")
+				//fmt.Printf("Applied incoming Raft entries\n")
 
 				confChanged := false
 				for _, ent := range rd.CommittedEntries {
