c210aabbb02ecc55315fd49c9252761930c193d4
Lucas <lucasphone75@gmail.com>
2025-07-03 01:36:31 -0600

    debug snapshots

--- server/etcdserver/server.go
+++ server/etcdserver/server.go
@@ -1222,7 +1222,7 @@ func (s *EtcdServer) snapshotIfNeededAndCompactRaftLog(ep *etcdProgress) {
 	if !shouldSnapshotToDisk && !shouldSnapshotToMemory {
 		return
 	}
-	//fmt.Printf("Snapshotting?\n")
+	fmt.Printf("Snapshotting?\n")
 	s.snapshot(ep, shouldSnapshotToDisk)
 	s.compactRaftLog(ep.appliedi)
 }
